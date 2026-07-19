7fa7dbc6f9bbd5e1a6b20ff3c1e6f479daae5776
Sudhanshu-2550 <swraj2007@gmail.com>
2026-07-19 17:01:46 +0530

    first python script with variables

---
+++ 01_data_science_and_ml/beginning.py
@@ -0,0 +1,12 @@
+# 1. Beginning with Data Science and Machine Learning   
+print("Welcome to the world of Data Science and Machine Learning!  SUDHANSHU")
+#2. Creating variables (storing data)
+name = "SUDHANSHU"
+age = 19
+height = 5.9
+print(f"\ndeveloper name is {name}, age is {age} and height is {height} feet")
+# 3. Doing some basics
+daily_learning_hours = 11
+days_in_a_week = 7
+weekly_learning_hours = daily_learning_hours * days_in_a_week
+print(f"\nWeekly learning hours: {weekly_learning_hours}")
