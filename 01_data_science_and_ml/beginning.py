f660511fedc606d3d3f082c7b401c1866368b123
Sudhanshu-2550 <swraj2007@gmail.com>
2026-07-20 23:41:51 +0530

    Did Leet Code question 1 TWO SUM

--- 01_data_science_and_ml/beginning.py
+++ 01_data_science_and_ml/beginning.py
@@ -1,4 +1,7 @@
 # 1. Beginning with Data Science and Machine Learning   
+from ast import List
+
+
 print("Welcome to the world of Data Science and Machine Learning!  SUDHANSHU")
 #2. Creating variables (storing data)
 name = "SUDHANSHU"
@@ -10,3 +13,16 @@ daily_learning_hours = 11
 days_in_a_week = 7
 weekly_learning_hours = daily_learning_hours * days_in_a_week
 print(f"\nWeekly learning hours: {weekly_learning_hours}")
+#Leet code question 1 TWO SUM
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}  # value -> index        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+        return []
+solution = Solution()
+print(solution.twoSum([2, 7, 11, 15], 9))  
